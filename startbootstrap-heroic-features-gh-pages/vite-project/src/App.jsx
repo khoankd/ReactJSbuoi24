e370570112dfee795335191e157061b7747b08ac
khoa.nguyen-kim <khoashyofme24092004@gmail.com>
2025-03-04 23:31:37 +0700

    Add package-lock.json and startbootstrap files

---
+++ startbootstrap-heroic-features-gh-pages/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { Navbar } from "./navbar.jsx";
+import { Banner } from "./Banner.jsx";
+import { Footer } from "./Footer.jsx";
+import { Item } from "./Item.jsx";
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Banner></Banner>
+    <Item></Item>
+    <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
---
+++ startbootstrap-heroic-features-gh-pages/vite-project/src/Footer.jsx
@@ -0,0 +1,16 @@
+import "../../css/styles.css";
+import "../../js/scripts.js";
+import "../../css/bootstrap.min.css";
+import "../../js/bootstrap.bundle.min.js";
+
+export function Footer() {
+  return (
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">
+          Copyright &copy; Your Website 2023
+        </p>
+      </div>
+    </footer>
+  );
+}
